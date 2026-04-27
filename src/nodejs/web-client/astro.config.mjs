5c0308b380488dc06719d496463322e74be1da83
Julio Raffaine <julio.raffaine@sintropiastudios.com>
2026-04-26 21:16:43 -0700

    feat(web-client): initialize Astro project and configure for GitHub Pages

---
+++ src/nodejs/web-client/astro.config.mjs
@@ -0,0 +1,7 @@
+import { defineConfig } from 'astro/config';
+
+// https://astro.build/config
+export default defineConfig({
+  site: 'https://SintropiaStudios.github.io',
+  base: '/king-cardgame',
+});
